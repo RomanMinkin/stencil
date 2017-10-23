69a5656726775adeaee4105f86209cb2ba65f418
Adam Bradley <adambradley25@gmail.com>
2017-10-23 10:29:47 -0500

    fix

--- src/core/instance/disconnected.ts
+++ src/core/instance/disconnected.ts
@@ -18,15 +18,6 @@ export function disconnectedCallback(plt: PlatformApi, elm: HostElement) {
     // that they're good to go and loaded (cuz this one is on its way out)
     propagateElementLoaded(elm);
 
-    // call instance Did Unload and destroy instance stuff
-    // if we've created an instance for this
-    const instance = elm.$instance;
-    if (instance) {
-      // call the user's componentDidUnload if there is one
-      instance.componentDidUnload && instance.componentDidUnload();
-      elm.$instance = instance.__el = null;
-    }
-
     // detatch any event listeners that may have been added
     // this will also set _listeners to null if there are any
     detachListeners(elm);
@@ -37,6 +28,15 @@ export function disconnectedCallback(plt: PlatformApi, elm: HostElement) {
     if (elm._hostContentNodes) {
       // overreacting here just to reduce any memory leak issues
       elm._hostContentNodes = elm._hostContentNodes.defaultSlot = elm._hostContentNodes.namedSlots = null;
+    }
+
+    // call instance Did Unload and destroy instance stuff
+    // if we've created an instance for this
+    const instance = elm.$instance;
+    if (instance) {
+      // call the user's componentDidUnload if there is one
+      instance.componentDidUnload && instance.componentDidUnload();
+      elm.$instance = instance.__el = null;
     }
 
     // fuhgeddaboudit
